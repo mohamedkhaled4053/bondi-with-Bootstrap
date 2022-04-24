0fe6863f16ae0dbf1838c39e585bd39bd117930d
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-04-24 07:57:07 +0200

    show navbar only when scrolling

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
+// Global variables
 let portfolioList = document.querySelector('.portfolio ul')
 let works = document.querySelectorAll('.portfolio .row .box')
+let mainHeader = document.querySelector('.navbar')
+let sections = document.querySelectorAll('body>div')
 
-
+// add a filter to the portfolio section
 portfolioList.addEventListener('click', (e) => {
     if (e.target.nodeName == 'LI') {
         let arr = Array.from(portfolioList.children);
+        // remove active state from any element and add to clicked one
         arr.forEach(el => el.classList.remove('active', 'rounded-pill'))
         e.target.classList.add('active', 'rounded-pill')
 
         works.forEach((work) => {
+            // hide all elements
             work.style.display = 'none'
             work.parentElement.classList.add('order-5')
+            // only show active element and sort it first
             if (work.dataset.work == e.target.textContent || e.target.textContent == 'all') {
                 work.style.display = 'block'
                 work.parentElement.classList.remove('order-5')
@@ -18,3 +24,25 @@ portfolioList.addEventListener('click', (e) => {
         })
     }
 })
+
+
+
+// show navbar only when scrolling
+// helper functions
+document.addEventListener('scroll', showAndHide)
+
+let timer;
+function showAndHide() {
+    showElement(mainHeader)
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        hideElement(mainHeader)
+    }, 3000);
+}
+
+function showElement(elem) {
+    elem.classList.remove('hidden')
+}
+function hideElement(elem) {
+    elem.classList.add('hidden')
+}
